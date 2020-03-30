1480d24e58c604515ce3d2abfb4f7f2356309947
rifki ibnu sholeh <62230886+riibnus28@users.noreply.github.com>
2020-03-30 19:43:39 +0700

    Add files via upload

---
+++ CoffeMechine.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCoffeMechine
+{
+    class CoffeMechine
+    {
+        private CoffePowder coffePowder;
+        private WaterGalon waterGalon;
+        private Milk miLk;
+
+        public CoffeMechine(CoffePowder powder, WaterGalon galon, Milk miLk)
+        {
+            this.waterGalon = galon;
+            this.coffePowder = powder;
+            this.miLk = miLk;
+        }
+
+        public String makeEsspresso()
+        {
+            if (!this.waterGalon.isAvailable())
+            {
+                return "sorry, the water is empty";
+            }
+            if (!this.coffePowder.isAvailable())
+            {
+                return "Sorry, the coffe is empty";
+            }
+
+
+            this.waterGalon.makeOneCup();
+            this.coffePowder.makeOneCup();
+            return "Yey! your coffe is ready";
+        } 
+        
+        public String makeCappucino()
+        {
+            if (!this.waterGalon.isavailable())
+            {
+                return "sorry, the water is empty";
+            }
+            if (!this.coffePowder.isAvailable())
+            {
+                return "Sorry, the coffe is empty";
+            }
+            if (!this.miLk.isAvailable())
+            {
+                return "Sorry, the milk is empty";
+            }
+
+
+            this.waterGalon.makeonecup();
+            this.coffePowder.makeOneCup();
+            this.miLk.makeonecup();
+            return "Yey! your cappucino is ready";
+        }
+      
+        public String checkAvailability()
+        {
+            return $"the water :{this.waterGalon.getVolume()} and the coffe powder : {this.coffePowder.getNetto()} and the milk : {this.miLk.getVolume()}";
+        }
+    }
+}
---
+++ WaterGalon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCoffeMechine
+{
+    class WaterGalon
+    {
+        private int volume = 0; //mililiter
+        private int volumeOneCup = 250;
+        private int volumeonecup = 150;
+
+        public WaterGalon (int volume)
+        {
+            this.volume = volume;
+        }
+
+        public Boolean isAvailable()
+        {
+            return volume >= volumeOneCup;
+        }
+
+        public Boolean isavailable()
+        {
+            return volume >= volumeonecup;
+        }
+        public void addWater (int volume)
+        {
+            this.volume += volume;
+        }
+
+        public int makeOneCup()
+        {
+            this.volume = this.volume - this.volumeOneCup;
+            return volumeOneCup;
+        }
+
+        public int makeonecup()
+        {
+            this.volume = this.volume - this.volumeonecup;
+            return volumeonecup;
+        }
+
+        public int getVolume()
+        {
+            return this.volume;
+        }
+    }
+}
